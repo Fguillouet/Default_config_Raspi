194e2e6806460542eb74014c432217a451d19feb
Fguillouet <franck.guillouet@outlook.fr>
2022-10-01 13:58:59 +0200

    Ajout du menu

--- Config_raspi.sh
+++ Config_raspi.sh
@@ -1,9 +1,32 @@
 #/bin/sh
+Menu
 function Menu {
-    echo "Prérequis : lancer la commande :tail -f /var/log/messages afin de voir quel est le nom du périphérique USB branché (exemple : sda1)"
-    echo "Mise a jour et installation des paquets de base"
-    echo "Config de la raspi"
-    echo "Vérifier le fonctionnement des services"
+    echo "Prérequis : lancer la commande : tail -f /var/log/messages afin de voir quel est le nom du périphérique USB branché (exemple : sda1)"
+    echo ""
+    echo "1 - Mise a jour et installation des paquets de base"
+    echo "2 - Config de la raspi"
+    echo "3 - Vérifier le fonctionnement des services"
+    echo "4 - Arret du script"
+    echo ""
+    read -p "Choisissez une option (1-4) : " Input_String
+    case $Input_String in 
+        1)
+            UpdateAndInstallAndEnable    
+        ;;
+        2)
+            ConfigRaspi
+        ;;
+        3)
+            CheckServices
+        ;;
+        4)
+            echo "!! Arrêt du script !!"
+            break
+        ;;
+        *)
+            echo "Entrée invalide, veuillez tapper un chiffre entre 1 et 4"
+        ;;
+        esac
 }
 
 function UpdateAndInstallAndEnable {
@@ -16,6 +39,11 @@ sudo apt install samba -y
 
 #Enable
 sudo service smbd enable
+
+clear
+
+#Retour au menu
+Menu
 } 
 
 function ConfigRaspi{
@@ -25,9 +53,17 @@ sudo mkdir /mnt/HDD
 sudo mount -t ntfs-3g /dev/$disk /mnt/HDD
 sudo chown -R pi:pi /mnt/HDD
 sudo chmod -R 755 /mnt/HDD
+
+clear 
+
+#Retour au menu
+Menu
 }
 
-function Check Services {
+function CheckServices {
 sudo service smbd status
 sudo service ntfs-3g status
+
+#Retour au menu
+Menu
 }
